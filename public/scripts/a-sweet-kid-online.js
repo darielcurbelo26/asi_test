003275b4819dc1bde0090d5035d720aeb4208a6f
darielcurbelo26 <ddavidcurbelo@gmail.com>
2026-03-18 09:39:06 +0100

    fix: check auth before loading gallery (redirect to password if not unlocked)

--- public/scripts/a-sweet-kid-online.js
+++ public/scripts/a-sweet-kid-online.js
@@ -1,3 +1,9 @@
-import * as THREE from 'https://unpkg.com/three@0.163.0/build/three.module.js';
+// --- AUTH CHECK ---
+if (sessionStorage.getItem('tatc-unlocked') !== 'true') {
+  window.location.href = 'password.html?redirect=a-sweet-kid-online.html';
+  throw new Error('Not authenticated');
+}
+
+import * as THREE from 'https://esm.sh/three@0.163.0';
 
 // The rest of the file content remains the same...
